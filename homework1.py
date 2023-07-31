7b97b9dfcfef4ef1b13167bdb42a3955d8fc7b73
3bdnaser <141034951+3bdnaser@users.noreply.github.com>
2023-07-31 15:39:10 +0300

    Create homework1.py

---
+++ homework1.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+dob = input("Enter your date of birth (YYYY-MM-DD): ")
+
+print("\nName:", name)
+print("Age:", age)
+print("Date of Birth:", dob)
