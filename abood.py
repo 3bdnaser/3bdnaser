5d6cc4d0487e9fce42ebc666a170e909c0bed829
3bdnaser <141034951+3bdnaser@users.noreply.github.com>
2023-08-02 16:18:54 +0300

    Create abood.py

---
+++ abood.py
@@ -0,0 +1,22 @@
+def add_numbers(a, b):
+    return a + b
+
+def subtract_numbers(a, b):
+    return a - b
+
+def multiply_numbers(a, b):
+    return a * b
+
+def divide_numbers(a, b):
+    return a/b
+
+num1 = 10
+num2 = 5
+
+print("Sum:", add_numbers(num1, num2))
+print("Difference:", subtract_numbers(num1, num2))
+print("Product:", multiply_numbers(num1, num2))
+print("Division:", divide_numbers(num1, num2))
+
+
+
